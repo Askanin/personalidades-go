ba0b5b1728eb56158ca77209f68295d011c29db3
Felipe Lima <flimas1985@gmail.com>
2024-09-09 11:40:15 -0300

    Implementacao do Middleware e configuracao do CORS para integracao com o frontend

--- routes/routes.go
+++ routes/routes.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	"github.com/Askanin/personalidade-go.git/controllers"
+	"github.com/Askanin/personalidade-go.git/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
